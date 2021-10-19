d6201b11d3eb26b48294fb971a2753fed21e5fca
SrMeliodas88 <77038531+SrMeliodas88@users.noreply.github.com>
2021-10-19 16:06:51 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,436 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Meta For browser SEO  --------------------->
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+		<meta name="application-name" content="Uincs">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="refresh" content="">
+		<meta name="genrator" content="">	
+	<!-- Set website icon for Microsoft IE --------->
+		
+		<meta name="msapplication-square310x310logo" content="favicon.png">	
+	<!-- Chrome tab color for chrome browser ------->
+		<meta name="theme-color" content="#FF0000">
+		<link rel="icon" sizes="192x192" href="favicon-192.png">
+	<!-- apple-touch-icons  ------------------------>
+		<link rel="apple-touch-icon" href="assets/apple-touch-icon-iphone-60x60.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/apple-touch-icon-ipad-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/apple-touch-icon-iphone-retina-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/apple-touch-icon-ipad-retina-152x152.png">
+	<!-- facebook share property ------------------->
+		<meta property="og:title" content="">
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+		<meta property="og:image" content="">
+	<!-- Page title -------------------------------->		
+		<title>Blog by KV | Uincs.com</title>
+	<!-- Import All stylesheat from css dir -------->
+		<link rel="stylesheet" href="css/style.css">
+	<!-- End here -->
+</head>
+<body class="" id="stop">
+
+<div class="search-tab hide-pos" id="search-tab">
+	<form action="?" method="get" class="icon-btn sr-form" enctype="multipart/form-data">
+	<img src="assets/exit.svg" alt="" onclick="hideSearchTab();">
+		<input type="text" name="Search" value="" placeholder="Search Uincs.com">
+		<button type="submit" class=""><img src="assets/search.svg" alt="" class="icon-btn"></button>
+	</form>
+</div>
+<header class="top musthead" id="top">
+	<div class="wraper">
+<!-- Open menu and close menu btn for phone --------------------------->
+		<div class="icon-btn">
+			<div class="hide" id="close" onclick="hideMenu();">
+				<img src="assets/close.svg" alt=""></div>
+			<div class="menu" id="menu" onclick="showMenu();">
+				<img src="assets/menu.svg" alt=""></div>
+		</div>
+
+	
+	<div class="logo">
+		<a href="home.php"><img src="assets/uincs.png" alt=""></a>
+	</div>
+	
+	<div class="phone-search icon-btn">
+		<img src="assets/search.svg" alt="" onclick="showSearchTab();">
+	</div>
+	
+<!-- Navigation menu links ------------------->
+	<nav class="hide-nav-con scrollbar fixdiv nav-con" id="navbar">
+		<div class="icon-btn">
+			<div class="" id="close" onclick="hideMenu();">
+				<img src="assets/close.svg" alt=""></div>
+		</div>
+			<ul class="nav-link">
+			 	<li><a class="active-link" href="home.php">Blog</a></li>
+			  	<li><a href="">Product</a></li>
+			  	<li><a href="">About</a></li>
+			  	<li><a href="">Contact</a></li>
+			  	<li><a href="">Policy</a></li>
+			  	<li class="icon-btn desk-search">
+			  	<img  src="assets/search.svg" alt="" onclick="showSearchTab();"></li>
+			</ul>
+<!-- .social-icon = Social icon links  ---------->
+		<div class="social-icon">
+			<ul>
+				<li><a href="https://youtube.com/channel/UCxcg9exYD2vJvsBmROHSj0w" target="_blank">
+					<img class="yt" src="assets/youtube-256.png" alt=""></a>
+				</li>
+				<li><a href="https://m.facebook.com/uincscom" target="_blank">
+					<img class="fb" src="assets/facebook-256.png" alt=""></a>
+				</li>	
+				<li><a href="https://www.pinterest.com/uincscom" target="_blank">
+					<img class="pn" src="assets/pinterest-256.png" alt=""></a>
+				</li>
+				<li><a href="https://www.twitter.com/uincscom" target="_blank">
+					<img class="tw" src="assets/twitter-256.png" alt="" ></a>
+				</li>
+				<li><a href="https://www.instagram.com/uincscom" target="_blank">
+					<img class="ig" src="assets/instagram-256.png" alt="" ></a>
+				</li>
+				<li><a href="https://www.linkedin.com/in/selkv" target="_blank">
+					<img class="in" src="assets/linkedin-256.png" alt=""></a>
+				</li>	
+			</ul>
+		</div>
+	</nav>
+		
+</div>
+</header>
+<div class="GapFix" style="padding:20px 0; scroll-behavior: smooth;" id="goup"></div>
+<div class="post-page">
+		<h1 class="post-sec-title">A Eye Catching Section TItle | Uincs.com</h1>
+	<div class="post-card card-con">
+		
+		<!-- Start No1 A single post card -->	
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/1.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/2.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/3.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/4.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/5.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/6.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/7.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/8.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/9.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/10.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/11.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/12.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/13.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/14.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+						<div class="card">
+					<a href="https://www.uincs.com/templates"></a>
+					<div class="thumb"style="background-image:url('assets/uincs-thumb.png');"> 
+							<img src="uploads/img/15.jpg" alt="">
+					 </div> 
+					<div class="content">
+					<div class="about"> 
+						<h3 class="title">Write A beautiful eye catching title for post card sample
+				 		</h3>
+						<p class="desc">Deccription Of The Lorem Ipsum Dolor Sit Content Lorem ipsum dolor 
+							sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore 
+							content Lorem ipsum dolor sit
+						</p>
+					</div>
+					<div class="info">
+						<a href="uincs.com"><span class="info-ico"></span>Category</a>
+						<a href="uincs.com"><span class="info-ico"></span>973K Views</a>
+					</div>
+				</div>
+			</div>
+					<!-- close one single card  -->
+
+			
+				
+		
+		 </div>  <!--  close cards container	  -->
+
+		<div class="pagination">
+				<ul>
+					<li><a href="" class="">Pre</a></li>
+					<li class="active"><a href="">2</a></li>
+					<li><a href="">3</a></li>
+					<li><a href="">4</a></li>
+					<li><a href="">5</a></li>
+					<li><a href="">Next</a></li>
+				</ul>
+		</div>
+</div>
+<!-- Footer Area -->
+<footer>
+	<a href="uincs.com/templates.php"><h2>Created by &copy;Uincs</h2></a>
+</footer>
+
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
