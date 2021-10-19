d8451998822b685d9c2eaf276d77d1e915024176
SrMeliodas88 <77038531+SrMeliodas88@users.noreply.github.com>
2021-10-19 16:31:28 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Meta For browser SEO  --------------------->
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+		<meta name="application-name" content="MultiverseMC">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="refresh" content="">
+		<meta name="genrator" content="">	
+	<!-- Set website icon for Microsoft IE --------->
+		
+		<meta name="msapplication-square310x310logo" content="favicon.png">	
+	<!-- Chrome tab color for chrome browser ------->
+		<meta name="theme-color" content="#FF0000">
+		<link rel="icon" sizes="192x192" href="favicon-192.png">
+	<!-- apple-touch-icons  ------------------------>
+		<link rel="apple-touch-icon" href="assets/apple-touch-icon-iphone-60x60.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/apple-touch-icon-ipad-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/apple-touch-icon-iphone-retina-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/apple-touch-icon-ipad-retina-152x152.png">
+	<!-- facebook share property ------------------->
+		<meta property="og:title" content="">
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+		<meta property="og:image" content="">
+	<!-- Page title -------------------------------->		
+		<title>Bedrockdocs</title>
+	<!-- Import All stylesheat from css dir -------->
+		<link rel="stylesheet" href="css/style.css">
+	<!-- End here -->
+</head>
+<body class="" id="stop">
+
+<div class="search-tab hide-pos" id="search-tab">
+	<form action="?" method="get" class="icon-btn sr-form" enctype="multipart/form-data">
+	<img src="assets/exit.svg" alt="" onclick="hideSearchTab();">
+		<input type="text" name="Search" value="" placeholder="Search Uincs.com">
+		<button type="submit" class=""><img src="assets/search.svg" alt="" class="icon-btn"></button>
+	</form>
+</div>
+<header class="top musthead" id="top">
+	<div class="wraper">
+<!-- Open menu and close menu btn for phone --------------------------->
+		<div class="icon-btn">
+			<div class="hide" id="close" onclick="hideMenu();">
+				<img src="assets/close.svg" alt=""></div>
+			<div class="menu" id="menu" onclick="showMenu();">
+				<img src="assets/menu.svg" alt=""></div>
+		</div>
+
+	
+	<div class="logo">
+		<a href="home.php"><img src="assets/uincs.png" alt=""></a>
+	</div>
+	
+	<div class="phone-search icon-btn">
+		<img src="assets/search.svg" alt="" onclick="showSearchTab();">
+	</div>
+	
+<!-- Navigation menu links ------------------->
+	<nav class="hide-nav-con scrollbar fixdiv nav-con" id="navbar">
+		<div class="icon-btn">
+			<div class="" id="close" onclick="hideMenu();">
+				<img src="assets/close.svg" alt=""></div>
+		</div>
+			<ul class="nav-link">
+			 	<li><a class="active-link" href="home.php">Blog</a></li>
+			  	<li><a href="">Product</a></li>
+			  	<li><a href="">About</a></li>
+			  	<li><a href="">Contact</a></li>
+			  	<li><a href="">Policy</a></li>
+			  	<li class="icon-btn desk-search">
+			  	<img  src="assets/search.svg" alt="" onclick="showSearchTab();"></li>
+			</ul>
+<!-- .social-icon = Social icon links  ---------->
+		<div class="social-icon">
+			<ul>
+				<li><a href="https://youtube.com/channel/UCwPeH8_BkXSeaeSLzN2_r6Q" target="_blank">
+					<img class="yt" src="assets/youtube-256.png" alt=""></a>
+				</li>
+				<li><a href="https://m.facebook.com/" target="_blank">
+					<img class="fb" src="assets/facebook-256.png" alt=""></a>
+				</li>
+				<li><a href="https://www.twitter.com/MultiversoMC" target="_blank">
+					<img class="tw" src="assets/twitter-256.png" alt="" ></a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+		
+</div>
+</header>
+<div class="GapFix" style="padding:20px 0; scroll-behavior: smooth;" id="goup"></div>
+<div class="post-page">
+		<h1 class="post-sec-title">Bedrock Addons and Docs</h1>
+	<div class="post-card card-con">
+
+			
+				
+		
+<!-- Footer Area -->
+<footer>
+	<a href="https://youtube.com/channel/UCwPeH8_BkXSeaeSLzN2_r6Q"><h2>Created by &copy;MultiverseMC</h2></a>
+</footer>
+
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
